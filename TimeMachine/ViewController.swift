6616a9f7b15be8a0461ac250547eab756ba4bd91
Denis <banddk1@gmail.com>
2015-01-29 01:46:08 +0300

    Start.I have done location update

--- TimeMachine/ViewController.swift
+++ TimeMachine/ViewController.swift
@@ -7,17 +7,77 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
+    @IBOutlet weak var TimeLabel: UILabel!
+    var manager:CLLocationManager!
+    var locationStatus : NSString = "Not Started"
+    var seenError : Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        GetChangeTime()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func GetChangeTime(){
+        manager.startUpdatingLocation();
+    }
+    func manager(manager: CLLocationManager!, didFailWithError error: NSError!) {// problem in location update
+        manager.stopUpdatingLocation()
+        if ((error) != nil) {
+            if (seenError == false) {
+                seenError = true
+                print(error)
+            }
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {//
+            var locationArray = locations as NSArray
+            var locationObj = locationArray.lastObject as CLLocation
+            var coord = locationObj.coordinate
+            
+            println(coord.latitude)
+            println(coord.longitude)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus) {//authorization status
+            var shouldIAllow = false
+            
+            switch status {
+            case CLAuthorizationStatus.Restricted:
+                locationStatus = "Restricted Access to location"
+            case CLAuthorizationStatus.Denied:
+                locationStatus = "User denied access to location"
+            case CLAuthorizationStatus.NotDetermined:
+                locationStatus = "Status not determined"
+            default:
+                locationStatus = "Allowed to location Access"
+                shouldIAllow = true
+            }
+            if (shouldIAllow == true) {
+                NSLog("Location to Allowed")
+                // Start location services
+                manager.startUpdatingLocation()
+            } else {
+                NSLog("Denied access: \(locationStatus)")
+            }
     }
 
 
